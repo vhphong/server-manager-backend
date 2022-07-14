e14f9883bc7504f87d0a0e9012fa103496b3f1a1
vhphong <vhphong@gmail.com>
2022-07-13 23:15:11 -0400

    added ServerService interface

---
+++ src/main/java/com/projects/servermanager/services/ServerService.java
@@ -0,0 +1,17 @@
+package com.projects.servermanager.services;
+
+import com.projects.servermanager.models.Server;
+
+import java.util.Collection;
+
+public interface ServerService {
+    Server createServer(Server server);
+
+    Collection<Server> listOfServerInLimit(int limit);
+
+    Server getServerById(Long id);
+
+    Server updateServer(Server server);
+
+    Boolean deleteServerById(Long id);
+}
